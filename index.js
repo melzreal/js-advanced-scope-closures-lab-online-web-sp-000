f82226afb53c5f41517ba3b6c7d60ade43e061e8
Melissa Real <melissa.real@hotmail.com>
2019-04-19 22:36:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+
+    let blocksDesired = Math.abs(parseInt(start) - parseInt(end));
+    let blocksTotal = blockRange - blocksDesired;
+
+    if (blocksTotal >= 0) {
+        return `within range by ${blocksTotal}`;
+    } else {
+        return `${Math.abs(blocksTotal)} blocks out of range`;
+    }
+  };
+}
+
+
+function produceTipCalculator(percent) {
+  return function(tip) {
+    return tip * percent;
+  };
+}
+
+function createDriver(){
+  let DriverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+  }
+}
