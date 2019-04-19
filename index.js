8f6846bb114048151f06b0f1833ab352a9314806
Melissa Real <melissa.real@hotmail.com>
2019-04-19 22:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function retailPriceMaker(produceDrivingRange) {
     return marketMultiplier - produceDrivingRange;
   };
 }
+
+
+function produceTipCalculator(percent) {
+  return function(tip) {
+    return tip * percent;
+  };
+}
